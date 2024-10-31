5fcd0157b2c095e088609f9769d269cb71410d4d
RenzoLaporno <renzogabriel-19@cpu.edu.ph>
2024-11-01 03:32:04 +0800

    Fix: deleted unused comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,6 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
-import { fetchN, page } from "./fetchdata";
+import { page } from "./fetchdata";
 
 export default function Home() {
-  // const [red, setRed] = useState(null);
-
-  // useEffect(() => {
-  //   Page().then(setRed).catch(console.error);
-  // }, []);
-
-  // const fetchData = async () => {
-  //   const data = await page(); // Await the promise
-  //   const districtCodes = data.districts.map(
-  //     (district: { district_code: any; }) => district.district_code
-  //   );
-
-  //   console.log(districtCodes);
-  // };
-  // fetchData();
-
-  // fetchN();
-
   return <button onClick={page}>Activate API</button>;
 }
