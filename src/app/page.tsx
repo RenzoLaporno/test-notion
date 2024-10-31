148247a6daf4668c636ef7ea32ccd7418f305cc6
CarloCruz <carlo.cruz.0755@gmail.com>
2024-11-01 03:17:13 +0800

    connected to frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-const fetchN = async () => {
-  const res = await fetch("http://localhost:3000/api/notion");
-  const data = await res.json();
-  // return JSON.parse(data);
-  return data
-};
+"use client";
 
-export default async function Home() {
-  const data = await fetchN();
-  return <div></div>;
+import React, { useEffect, useState } from "react";
+import { fetchN, page } from "./fetchdata";
+
+export default function Home() {
+  // const [red, setRed] = useState(null);
+
+  // useEffect(() => {
+  //   Page().then(setRed).catch(console.error);
+  // }, []);
+
+  // const fetchData = async () => {
+  //   const data = await page(); // Await the promise
+  //   const districtCodes = data.districts.map(
+  //     (district: { district_code: any; }) => district.district_code
+  //   );
+
+  //   console.log(districtCodes);
+  // };
+  // fetchData();
+
+  // fetchN();
+
+  return <button onClick={page}>Activate API</button>;
 }
